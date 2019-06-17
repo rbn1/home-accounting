35a788d1ee84870386155754840f2a5f1df78eda
Ruben <rubengs.94@gmail.com>
2019-06-17 17:24:55 +0200

    new commit

---
+++ src/test/java/com/rbn/HomeAccountingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeAccountingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
